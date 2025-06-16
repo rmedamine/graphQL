d39fa724779de049979736e59a4e3f29ff3a8359
rmohamme <raoui.med.amine@gmail.com>
2025-06-16 03:17:44 +0100

    latest v.0.300

--- src/js/header.js
+++ src/js/header.js
@@ -6,12 +6,9 @@ export  function headers() {
     let container = document.querySelector(".container")
     let headers = DivsIndes("headers")
     let header = DivsIndes("header")
-    let h1 = document.createElement("h1")
     let button = document.createElement("button")
-    h1.textContent = "Data Overview"
     button.textContent = "Log Out"
     button.classList = "button"
-    header.appendChild(h1)
     headers.append(header, button)
     container.appendChild(headers)
     logout()
